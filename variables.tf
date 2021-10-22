5b7725d1ad545d3cba8254e74380525d0c70f221
nbeam85 <92865313+nbeam85@users.noreply.github.com>
2021-10-22 13:04:21 -0400

    gave defaults to all variables for morpheus

--- variables.tf
+++ variables.tf
@@ -1,41 +1,51 @@
 variable "enviro" {
   type        = string
   description = "define the environment ex. dev,tst,prd,stg"
+  default     = "dev"
 }
 
 variable "prjname" {
   type        = string
   description = "define the project name ex. prj02"
+  default     = "nbdemo"
 }
 
 variable "prjnum" {
   type        = string
   description = "define the project number for TFstate file ex. 4858"
+  default     = "1111"
 }
 
 variable "location" {
   type        = string
   description = "location of your resource group"
+  default     = "usgovvirginia"
 }
 
 variable "orgname" {
   type        = string
+  default     = "tcld"
 }
 
 variable "vnet_hub_address_spaces" {
   type        = list
+  default     = ["192.168.145.0/24"]
 }
 variable "vnet_hub_dns_servers" {
   type        = list
+  default     = ["8.8.8.8", "1.1.1.1"]
 }
 variable "azfw_subnet_prefixes" {
   type        = list
+  default     = ["192.168.145.0/26"]
 }
 variable "bastion_subnet_prefixes" {
   type        = list
+  default     = ["192.168.145.64/26"]
 }
 variable "gateway_subnet_prefixes" {
   type        = list
+  default     = ["192.168.145.128/27"]
 }
 
 variable "resourcegroupnames" {
